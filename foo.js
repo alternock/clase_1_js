e036f370f519cae43d5c5e456e892ef3b5694a82
alternock <montilla269@gmail.com>
2022-08-31 00:14:31 -0300

    fc

---
+++ foo.js
@@ -0,0 +1,13 @@
+let nombre = "sarah";
+var edad = 10;
+let mayorDeEdad = 21;
+let NRO_PASAPORTE = 23445265;
+console.log("nro de pasaporte:",NRO_PASAPORTE);
+
+nombre = "terminator";
+NRO_PASAPORTE = 424562567;
+
+console.log("nombre:",nombre);
+console.log("edad:",edad);
+console.log("mayor de edad:",mayorDeEdad);
+console.log("nro de pasaporte:",NRO_PASAPORTE);
